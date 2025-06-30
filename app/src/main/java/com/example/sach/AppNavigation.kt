e0a5d8a34a8f268bcdeab92b480d0c0ac3e18b1a
Toan2004-ai <toanb2203481@student.ctu.edu.vn>
2025-06-30 22:25:40 +0700

    Lần đầu đẩy code từ Android Studio

---
+++ app/src/main/java/com/example/sach/AppNavigation.kt
@@ -0,0 +1,12 @@
+package com.example.sach
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavigation(){
+    val navController = rememberNavController()
+    val bookViewModel: BookViewModel = viewModel()
+    AppNavHost(navController = navController, bookViewModel = bookViewModel)
+}
---
+++ app/src/main/java/com/example/sach/NavigationItem.kt
@@ -0,0 +1,13 @@
+package com.example.sach
+
+enum class Sceen{
+    GET_START,
+    HOME,
+    SAVED
+}
+
+sealed class NavigationItem(val route: String){
+    object GET_START: NavigationItem(Sceen.GET_START.name)
+    object HOME: NavigationItem(Sceen.HOME.name)
+    object SAVED: NavigationItem(Sceen.SAVED.name)
+}
