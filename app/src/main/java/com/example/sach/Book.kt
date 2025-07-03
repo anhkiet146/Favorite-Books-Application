372e9a59c28b44afd574f0ccca8ae763e01089be
B2203472 <thienb2203472@student.ctu.edu.vn>
2025-07-03 23:02:16 +0700

    Initial commit on branch thien

---
+++ app/src/main/java/com/example/sach/Book.kt
@@ -0,0 +1,10 @@
+package com.example.sach
+
+data class Book(
+    val id: Int,
+    val tensach: String,
+    val tacgia: String,
+    val namxb: String,
+    val hinhanh: Int,
+    val favorite: Boolean = false
+)
---
+++ app/src/main/java/com/example/sach/FakeBookData.kt
@@ -0,0 +1,74 @@
+package com.example.sach
+
+fun DSsach(): List<Book> = listOf(
+    Book(
+        id = 1,
+        tensach = "Nhật ký crush bạn cùng bàn",
+        tacgia = "Mia",
+        namxb = "2025",
+        hinhanh = R.drawable.nhatky
+    ),
+    Book(
+        id = 2,
+        tensach = "Đồi gió hú",
+        tacgia = "Ellis Bell",
+        namxb = "1847",
+        hinhanh = R.drawable.doigiohu
+    ),
+    Book(
+        id = 3,
+        tensach = "Nhà giả kim",
+        tacgia = "Paulo Coelho",
+        namxb = "1988",
+        hinhanh = R.drawable.nh__gi_kim
+    ),
+    Book(
+        id = 4,
+        tensach = "Suối nguồn",
+        tacgia = "Ayn Rand",
+        namxb = "1943",
+        hinhanh = R.drawable.suoinguon
+    ),
+    Book(
+        id = 5,
+        tensach = "Đắc nhân tâm",
+        tacgia = "Dale Carnegie",
+        namxb = "1950",
+        hinhanh = R.drawable.c_nh_n_t_m
+    ),
+    Book(
+        id = 6,
+        tensach = "Cánh đồng bất tận",
+        tacgia = "Nguyễn Ngọc Tư",
+        namxb = "2005",
+        hinhanh = R.drawable.canh_dong_bat_tan
+    ),
+    Book(
+        id = 7,
+        tensach = "Tôi tài giỏi, bạn cũng thế!",
+        tacgia = "Adam Khoo",
+        namxb = "2019",
+        hinhanh = R.drawable.toi_tai_gioi
+    ),
+    Book(
+        id = 8,
+        tensach = "Bố già",
+        tacgia = "Mario Puzo",
+        namxb = "1969",
+        hinhanh = R.drawable.bo_gia_poster
+    ),
+    Book(
+        id = 9,
+        tensach = "Thép đã tôi thế đấy",
+        tacgia = "Nikolai A. Ostrovsky",
+        namxb = "1932",
+        hinhanh = R.drawable.th_p___t_i_th_y__
+    ),
+    Book(
+        id = 10,
+        tensach = "Những Tấm Lòng Cao Cả",
+        tacgia = "Edmondo De Amicis",
+        namxb = "1886",
+        hinhanh = R.drawable.nhung_tam_long_cao_ca
+    ),
+)
